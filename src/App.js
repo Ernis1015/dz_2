19c143b3ca3e35b2e4d3f3e998daec6a14f06f6b
Ernis1015 <156911113+Ernis1015@users.noreply.github.com>
2024-05-14 21:20:35 +0600

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,11 @@
+
+import MainPage from "../../dz_2/src/pages/mainPage/MainPage";
+function App() {
+  return (
+      <div className="App">
+        <MainPage/>
+      </div>
+  );
+}
+
+export default App;
